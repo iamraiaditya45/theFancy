2cab8ddeae62cf5f4143e85f1867da3de5209e2a
iamraiaditya45 <aditya.rai@appinventiv.com>
2022-02-10 18:13:29 +0530

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Appbar2 from './Component/Appbar2';
 import Dropdown from './Component/Dropdown';
 import Drawer from './Component/Footer';
 import Justin from './Component/JustIn';
-import Page from './Component/Pagination';
+import Page from './Component/pagination1';
 function App() {
   return (
     <div className="App">
